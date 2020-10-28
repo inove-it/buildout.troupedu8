2c04a4f50f26f7a37a313b20910d70fd4cfafda3
FBruynbroeck <francois.bruynbroeck@hotmail.com>
2020-10-28 16:38:52 +0100

    Add livesearch faceted-text-widget

--- src/troupedu8/common/theme/scripts/main.js
+++ src/troupedu8/common/theme/scripts/main.js
@@ -1,3 +1,16 @@
 $('.carousel').carousel({
         interval: 5000 //changes the speed
     });
+
+var delayTimer;
+jQuery('div.faceted-text-widget').each(function(){
+  var wid = jQuery(this).attr('id').split('_')[0];
+  var input = jQuery("#" + wid);
+  var widget = jQuery('#' + wid + '_widget');
+  var button = jQuery('input[type=submit]', widget);
+  button.hide();
+  input.on('input', function() {
+    clearTimeout(delayTimer);
+    delayTimer = setTimeout(function() {input.change();}, 300);
+  });
+});
